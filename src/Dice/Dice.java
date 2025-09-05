a3f33e1bad5773d2d7290fa380faf9c72939b52c
DozoEK <lask0005@stud.ek.dk>
2025-09-05 15:22:33 +0200

    0.3

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -4,15 +4,18 @@ import java.util.Random;
 
 public class Dice {
     private Random rand = new Random();
-    int dice = rand.nextInt(6) + 1;
+    int dice = (int)(Math.random() * 6) +1;
 
     public void rollUntilFive() {
+        int rollcounter = 1;
         int roll = 0;
-        while (roll != 6 ) {
+        while (rollcounter < 6) {
             roll = rand.nextInt(6) + 1;
             System.out.println("Rolled: " + roll);
+            rollcounter++;
         }
 
-        System.out.println("You rolled all the dice!");
+        System.out.println("EK DiceMan rolled all the dice!");
     }
+
 }
